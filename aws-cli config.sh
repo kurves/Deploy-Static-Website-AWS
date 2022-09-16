91f14f497df913ff83661e585d23fbd2879c8c60
kurves <kavssimply@gmail.com>
2022-09-16 23:46:29 +0300

    Update aws-cli config.sh

--- aws-cli config.sh
+++ aws-cli config.sh
@@ -12,3 +12,7 @@ cd deploy-static-website-aws
 
 aws s3api put-object --bucket my-bucket-202203081 --key index.html --body index.html
 
+
+aws s3 cp vendor/ s3://my-bucket-202203081/vendor/ --recursive 
+aws s3 cp css/ s3://my-bucket-202203081/css/ --recursive 
+aws s3 cp img/ s3://my-bucket-202203081/img/ --recursive 
